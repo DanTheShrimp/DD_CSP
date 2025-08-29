36e4dd9e455dea10f48859b432a74bb0c23fddd1
DanTheShrimp <daniel.delong@ucas-edu.com>
2025-08-29 14:42:46 -0600

    variable notes

---
+++ Practices/variables_practice.py
@@ -0,0 +1,14 @@
+print("absolute test for printing")
+
+multi_digit = 19
+single_digit = 4
+
+print(multi_digit)
+
+multi_digit = 6942
+
+print(multi_digit)
+
+getted_scammed_idiot = input("Who are you and what is your bank information")
+
+print (getted_scammed_idiot)
