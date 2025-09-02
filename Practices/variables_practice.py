8cc2bf4c7e9b5817a04dfc0218d0a36e7d0b2ea0
DanTheShrimp <daniel.delong@ucas-edu.com>
2025-09-02 14:28:58 -0600

    Variables update... again

--- Practices/variables_practice.py
+++ Practices/variables_practice.py
@@ -1,12 +1,23 @@
 #DD Period 7 Variables practice
 
-user_name = "daniel"
+user_name = "Daniel"
 small_number = 7
 big_number = 983
-breakfast = "cinnamon oat cheerios"
-favorite_color = "orange"
+breakfast = "Cinnamon oat cheerios"
+favorite_color = "Orange"
 school = "UCAS"
 year = 2025
-eye_color = "blue"
+eye_color = "Blue"
 age = 14
-favorite_subject = "computers"
+favorite_subject = "Computers"
+
+print(user_name)
+print(small_number)
+print(big_number)
+print(breakfast)
+print(favorite_color)
+print(school)
+print(year)
+print(eye_color)
+print(age)
+print(favorite_subject)
--- Programs/first_python.py
+++ Programs/first_python.py
@@ -2,4 +2,6 @@
 
 user_name = input("Hello human. What string of characters would you like me to call you to get your attention?")
 
-print("Hello, ")(user_name)(", I would like your attention now.")
+print("Hello")
+print(user_name)
+print("I would like your attention now.")
