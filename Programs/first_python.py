6bcce2d8bfdd609e92a4e857d9cfd2b0903267c4
DanTheShrimp <daniel.delong@ucas-edu.com>
2025-09-03 14:06:17 -0600

    Made code better

--- Programs/first_python.py
+++ Programs/first_python.py
@@ -2,6 +2,4 @@
 
 user_name = input("Hello human. What string of characters would you like me to call you to get your attention?")
 
-print("Hello")
-print(user_name)
-print("I would like your attention now.")
+print("Hello", user_name, "I would like you attention now.")
