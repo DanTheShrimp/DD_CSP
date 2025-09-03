b3de591c78afb7012c3b82700e599029b45c502f
Shrimp <daniel.delong@ucas-edu.net>
2025-09-03 20:40:33 +0000

    !

--- Programs/first_python.py
+++ Programs/first_python.py
@@ -3,4 +3,4 @@
 user_name = input("Hello human. What string of characters would you like me to call you to get your attention?")
 
 print("Hello", user_name, "I would like your attention now.")
-print("Your mom, haha, I heard that was a big humor thing here")
+print("Your mom, haha, I heard that was a big humor thing here!")
