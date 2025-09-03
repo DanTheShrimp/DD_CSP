044737bc7be6ef10c9dfaab8bc4bfbf10354ccc4
Shrimp <daniel.delong@ucas-edu.net>
2025-09-03 20:26:50 +0000

    your

--- Programs/first_python.py
+++ Programs/first_python.py
@@ -2,4 +2,4 @@
 
 user_name = input("Hello human. What string of characters would you like me to call you to get your attention?")
 
-print("Hello", user_name, "I would like you attention now.")
+print("Hello", user_name, "I would like your attention now.")
