a89d3050f07b4fb61df490b91221e681937d5ddf
DanTheShrimp <daniel.delong@ucas-edu.com>
2025-09-03 14:05:15 -0600

    calculator

---
+++ Fun!/calculator.py
@@ -0,0 +1,17 @@
+#DD Period 7 for fun :D
+
+first_number = input("What is the first number?")
+middle_thing = input("What is the middle thing? (+,-,/, or *)")
+last_number = input("What is the last number?")
+
+if middle_thing == "+":
+    print(first_number + last_number)
+
+elif middle_thing == "-":
+    print(first_number - last_number)
+
+elif middle_thing == "/":
+    print(first_number / last_number)
+
+elif middle_thing == "*":
+    print(first_number * last_number)
