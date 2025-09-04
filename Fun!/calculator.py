9532f4d2a380b0891e8e0707f742b8f2b74ab565
DanTheShrimp <daniel.delong@ucas-edu.com>
2025-09-04 14:41:19 -0600

    FIXED THINGS

--- Fun!/calculator.py
+++ Fun!/calculator.py
@@ -1,17 +1,17 @@
 #DD Period 7 for fun :D
 
-first_number = input("What is the first number?")
-middle_thing = input("What is the middle thing? (+,-,/, or *)")
-last_number = input("What is the last number?")
+first_number = int(input("What is the first number?"))
+middle_thing = input("What is the operation? (+,-,/, or *)")
+last_number = int(input("What is the last number?"))
 
 if middle_thing == "+":
-    print(first_number + last_number)
+    print(first_number+last_number)
 
 elif middle_thing == "-":
-    print(first_number - last_number)
+    print(first_number-last_number)
 
 elif middle_thing == "/":
-    print(first_number / last_number)
+    print(first_number/last_number)
 
 elif middle_thing == "*":
-    print(first_number * last_number)
+    print(first_number*last_number)
---
+++ Practices/expressions_practice.py
@@ -0,0 +1,32 @@
+#DD Period 7 Expressions Practice
+
+name = input("What is your name? Tell me now.\n")
+print("Hello", name, "I need something from you.")
+print("...")
+print("...")
+print("...")
+print("...")
+print("...")
+print("...")
+print("...")
+print("...")
+print("Your skin.")
+
+skin = 500
+totally_human = 3
+print("Each 'human' gets", skin/totally_human, " of your skin!" )
+
+amount_of_friends = 4
+
+friend1 = int(input("How old is your first friend?\n"))
+friend2 = int(input("How old is your second friend?\n"))
+friend3 = int(input("How old is your third friend?\n"))
+friend4 = int(input("How old is your fourth friend?\n"))
+
+print ("[NERD MODE ACTIVATED]")
+print("...")
+print("...")
+print("...")
+print("...")
+print("...")
+print("The average age of your friend group is", (friend1 + friend2 + friend3 + friend4)/4, " wow!")
