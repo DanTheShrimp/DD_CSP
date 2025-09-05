05d0436412c7425bd2be51b8a598a6ac20f3cb49
DanTheShrimp <daniel.delong@ucas-edu.com>
2025-09-05 13:58:42 -0600

    \n

--- Fun!/calculator.py
+++ Fun!/calculator.py
@@ -1,8 +1,8 @@
 #DD Period 7 for fun :D
 
-first_number = int(input("What is the first number?"))
-middle_thing = input("What is the operation? (+,-,/, or *)")
-last_number = int(input("What is the last number?"))
+first_number = int(input("What is the first number?\n"))
+middle_thing = input("What is the operation? (+,-,/, or *)\n")
+last_number = int(input("What is the last number?\n"))
 
 if middle_thing == "+":
     print(first_number+last_number)
