eb661d9f093c67ea961fb93bdcc2afbf30697b73
Shrimp <daniel.delong@ucas-edu.net>
2025-09-06 00:57:47 +0000

    Changed name and added so much

--- Practices/financial_calculator.py
+++ Practices/financial_calculator.py
@@ -1,5 +1,6 @@
 #DD Period 7 Financial Calculator
 
+import sys #this is going to be used later on
 print("Hello! I am your personal financial calculator. Please type in all of the information I require.")
 
 income = float(input("How much do you make each month?\n"))
@@ -14,6 +15,19 @@ income_left_after_savings = income*0.9
 income_left = income_left_after_savings-(rent+utilities+groceries+transportation)
 total_expenses = rent+utilities+groceries+transportation
 percentage_expenses = total_expenses/income_left_after_savings
+if income_left < total_expenses:
+    print("I am unable to help you. Your finances are in ruins.")
+    end = 1
+
+elif income_left > total_expenses:
+    end = 0
+
+if end == 1:
+    sys.exit() #this is so we can stop the program from continuing if they are spending more than they earn
+
+if total_expenses == 0:
+    print("You must be lying! I refuse to help you.")
+    sys.exit() #fool proofing. no lying allowed...
 
 print("You will be saving $", round(savings, 2), "every month!")
 print("You will have $", round(income_left, 2), "after all of your expenses.")
@@ -21,10 +35,10 @@ print("That means you will be saving $", round(savings*12, 2), "each year!")
 print("And you will have $", round(income_left*12, 2), "to spend each year." )
 
 if percentage_expenses == 50:
-    print("Nice job, or not. You are spending ", percentage_expenses, "of your monthly income on expenses.")
+    print("Nice job, or not. You are spending ", percentage_expenses, "% of your monthly income on expenses.")
 
 elif percentage_expenses < 50:
-    print("Also, you are spending", round(percentage_expenses,2), "of your monthly income on expenses. Nice job! That isn't that much!")
+    print("Also, you are spending", round(percentage_expenses,2), "% of your monthly income on expenses. Nice job! That isn't that much!")
 
 elif percentage_expenses > 50:
-    print("You need to get help. Your monthly expenses are", round(percentage_expenses, 2), "of your monthly income.")
+    print("You need to get help. Your monthly expenses are", round(percentage_expenses, 2),"% of your monthly income.")
