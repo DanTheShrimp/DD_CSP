cff2327d78c267a7669ca02b2531c611498cdd32
Shrimp <daniel.delong@ucas-edu.net>
2025-09-08 18:42:18 +0000

    Argh matey

--- Fun!/calculator.py
+++ Fun!/calculator.py
@@ -1,7 +1,7 @@
 #DD Period 7 for fun :D
 
 first_number = int(input("What is the first number?\n"))
-middle_thing = input("What is the operation? (+,-,/, or *)\n")
+middle_thing = input("What is the operation? (+,-,/, *, or **)\n")
 last_number = int(input("What is the last number?\n"))
 
 if middle_thing == "+":
@@ -15,3 +15,6 @@ elif middle_thing == "/":
 
 elif middle_thing == "*":
     print(first_number*last_number)
+
+elif middle_thing == "**":
+    print(first_number**last_number)
