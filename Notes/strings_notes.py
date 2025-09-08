6cf0b7510987c331ebe3c72360ebdd3e07ee23d1
DanTheShrimp <daniel.delong@ucas-edu.com>
2025-09-08 14:41:40 -0600

    strings notes

--- Notes/strings_notes.py
+++ Notes/strings_notes.py
@@ -1,1 +1,13 @@
-#DD Period 7 strings somthin :/
+#DD Period 7 Strings Notes
+
+import time
+#Examples
+name = input("WHO ARE YOU?!\n").strip().title()
+print("Oh, you're", name + ", hello!")
+
+time.sleep(3)
+sentence = 'The quick brown fox jumps over the lazy dog.'
+print(sentence)
+
+time.sleep(2)
+print("Welcome to my program, HUMAN!!!")
