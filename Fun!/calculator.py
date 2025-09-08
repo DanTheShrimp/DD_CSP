29d78d09d03897aa7c0fae647862ab1778f813c9
DanTheShrimp <daniel.delong@ucas-edu.com>
2025-09-08 14:06:41 -0600

    RAAAAARRGGHHHH!!!!!!!!

--- Fun!/calculator.py
+++ Fun!/calculator.py
@@ -1,5 +1,13 @@
 #DD Period 7 for fun :D
 
+import random
+import sys
+
+random_number = random.randint(1, 10)
+if random_number == 7:
+    print("RAAAAARRGGHHHH!!!!!!!!")
+    sys.exit()
+
 first_number = int(input("What is the first number?\n"))
 middle_thing = input("What is the operation? (+,-,/, *, or **)\n")
 last_number = int(input("What is the last number?\n"))
