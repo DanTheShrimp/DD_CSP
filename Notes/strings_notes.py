ccf52c9e57653017c04207db730a628e343b09eb
DanTheShrimp <daniel.delong@ucas-edu.com>
2025-09-09 14:40:32 -0600

    Finished strings notes

--- Notes/strings_notes.py
+++ Notes/strings_notes.py
@@ -7,11 +7,13 @@ print("Oh, you're", name + ", hello!")
 
 time.sleep(3)
 sentence = 'The quick brown fox jumps over the lazy dog.'
-print(sentence)
-
+print(sentence.find("jumps"))
+print(sentence[20:25]) #slicing
+print(sentence[sentence.find("lazy"): len("lazy")+sentence.find("lazy")])
 time.sleep(2)
 print("Welcome to my program, HUMAN!!!")
 
 #sanitization and stupid proofing!1!11!1!1!1!11!!!1!
+#sanitization is cleansing things like capitals, lowercases, numbers, etc (this is step one of stupid proofing)
 
 #debugging is fixing code!1!!111!1!!!1!
