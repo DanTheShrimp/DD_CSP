c3ec0221d59a9256da6800a7f4ece7a0f1a77d89
DanTheShrimp <daniel.delong@ucas-edu.com>
2025-09-10 14:42:02 -0600

    NAME DECOR

---
+++ Programs/name_decorator.py
@@ -0,0 +1,64 @@
+#DD Period 7 Name Decorator
+
+import time
+name = input("Good day user. I am the Name Decorator. Please input your name. ").title()
+print(f"Thank you {name}, I will now generate examples for your custom name.")
+time.sleep(1)
+print("...")
+time.sleep(1)
+print("...")
+time.sleep(1)
+print("...")
+
+print("Thank you for waiting. Here are your base options.")
+print(f"1. (:< {name} >:)")
+time.sleep(1)
+print(f"2. 123{name}456")
+time.sleep(1)
+print(f"3. www{name}www")
+time.sleep(1)
+print(f"4. ._.{name}._.")
+time.sleep(1)
+print(f"5. UwU{name}UwU")
+time.sleep(1)
+print(f"6. D:{name}:D")
+time.sleep(1)
+
+more = input("If you are unhappy with the premade options, please type 'More'. If you like those six above, please type the number you like. ").capitalize()
+
+if more == "More":
+    extra = input("I see. Please type the symbol you would like to decorate your name with. ")
+    print(f"{extra}{extra}{extra}{name}{extra}{extra}{extra}")
+
+else:
+    print("Please wait.")
+    time.sleep(3)
+    if more == "1":
+        print("Here is your custom name.")
+        time.sleep(1)
+        print(f"(:< {name} >:)")
+
+    elif more == "2":
+        print("Here is your custom name.")
+        time.sleep(1)
+        print(f"123{name}456")
+
+    elif more == "3":
+        print("Here is your custom name.")
+        time.sleep(1)
+        print(f"www{name}www")
+
+    elif more == "4":
+        print("Here is your custom name.")
+        time.sleep(1)
+        print(f"._.{name}._.")
+
+    elif more == "5":
+        print("Here is your custom name.")
+        time.sleep(1)
+        print(f"UwU{name}UwU")
+
+    elif more == "6":
+        print("Here is your custom name.")
+        time.sleep(1)
+        print(f"D:{name}:D")
