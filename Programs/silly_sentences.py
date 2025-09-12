0ff4607dfbcfed54db5776838e5c80f691ccd215
Shrimp <daniel.delong@ucas-edu.net>
2025-09-12 01:38:33 +0000

    Silly sentences

--- Programs/silly_sentences.py
+++ Programs/silly_sentences.py
@@ -20,13 +20,13 @@ if randint == 1:
     print(f"You and your trusty {noun1} are {adverb1} {verb1} from {noun2}, and must find the {adjective1} statue that loves to {verb2}.")
 
 elif randint == 2:
-    print(f"The legendary {noun1} is in the {adjective1} hands of the evil {noun2} that will stop at nothing to {adverb1} {verb2} the world.")
+    print(f"The legendary {noun1} is in the {adjective1} hands of the {verb1}, evil {noun2} that will stop at nothing to {adverb1} {verb2} the world.")
 
 elif randint == 3:
-    print(f"You must rescue the {adjective1} king from the {adverb1}, evil {noun1}, which will destroy the {noun2} if you do not stop them.")
+    print(f"You must rescue the {adjective1} king from the {adverb1}, evil {verb1} {noun1}, which will {verb2} the {noun2} if you do not stop them.")
 
 elif randint == 4:
     print(f"I, the {adjective1} {noun1}, will protect you, my {adverb1} {verb1} {noun2}, lest you {verb2} away from me.")
 
 elif randint == 5:
-    print(f"You are renouned {adjective1} {noun1}, who always manages to send those {adverb1} {verb1} {noun2}s running.")
+    print(f"You are renouned {adjective1} {noun1}, who always manages to send those {adverb1} {verb1} {noun2}s {verb2}ing.")
