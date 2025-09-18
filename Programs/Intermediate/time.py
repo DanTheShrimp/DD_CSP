1b0e8b2de58522e6df2d9394729a5784fe29dc7e
DanTheShrimp <daniel.delong@ucas-edu.com>
2025-09-18 14:29:15 -0600

    trying to fail

--- Programs/Intermediate/time.py
+++ Programs/Intermediate/time.py
@@ -1,13 +1,19 @@
 #DD Period 7 Time of Day
 
-time = int(input("Please tell me the time. I am suffering because importing time is not working.\n"))
-if time >= 20 and time <= 24 or time < 5:
-    print("Why are you still awake? Go to sleep!")
-elif time >= 5 and time < 12:
-    print("Good morning!")
-elif time >= 12 and time < 18:
-    print("Good afternoon!")
-elif time >= 18 and time < 20:
-    print("Good evening!")
+import sys
+try:
+    time = int(input("Please tell me the time. I am suffering because importing time is not working.\n"))
+except ValueError:
+    print("I fudging hate you... I JUST WANT MY DAMN CODE TO WORK!! WHY DO YOU HAVE TO MAKE IT SO HARD FOR ME?!?!")
+    sys.exit()
 else:
-    print("I hate you... so so much... All I want is my code to work :(")
+    if time >= 20 and time <= 24 or time < 5:
+        print("Why are you still awake? Go to sleep!")
+    elif time >= 5 and time < 12:
+        print("Good morning!")
+    elif time >= 12 and time < 18:
+        print("Good afternoon!")
+    elif time >= 18 and time < 20:
+        print("Good evening!")
+    else:
+        print("I hate you... so so much... All I want is my code to work :(")
