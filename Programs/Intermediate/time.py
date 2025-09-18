c1528eee1b20e14e9cbcb2815390141acbeb80b8
Shrimp <daniel.delong@ucas-edu.net>
2025-09-18 18:44:47 +0000

    Moved things and did time.py

---
+++ Programs/Intermediate/time.py
@@ -0,0 +1,13 @@
+#DD Period 7 Time of Day
+
+time = int(input("Please tell me the time. I am suffering because importing time is not working.\n"))
+if time >= 20 and time <= 24 or time < 5:
+    print("Why are you still awake? Go to sleep!")
+elif time >= 5 and time < 12:
+    print("Good morning!")
+elif time >= 12 and time < 18:
+    print("Good afternoon!")
+elif time >= 18 and time < 20:
+    print("Good evening!")
+else:
+    print("I hate you... so so much... All I want is my code to work :(")
