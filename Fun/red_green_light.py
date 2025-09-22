2d9aef21d037e464459b70ebbb009c165eb78dd2
Shrimp <daniel.delong@ucas-edu.net>
2025-09-22 16:50:39 +0000

    [insert squid game doll chant here]

--- Fun/red_green_light.py
+++ Fun/red_green_light.py
@@ -2,6 +2,7 @@
 
 import time
 import random
+import sys
 def doll():
     towards = [
         "/----\\",
@@ -15,9 +16,62 @@ def doll():
     ]
     for item in towards:
         print(item)
+    print("Green light in 3")
     time.sleep(3)
-    sleep = random.randint(5, 10)
-    facing = 1
     for item in away:
         print(item)
+    facing_away = True
+    print("PRESS E TO RUN")
+    e = input("").strip().upper()
+    if e == "E":
+        advanced_player = 0
+        advanced_player = advanced_player+1
+    print("Red light in 1")
+    time.sleep(1)
+    for item in towards:
+        print(item)
+    facing_away = False
+    random_time = random.randint(1, 5)
+    print(f"Green light in {random_time}")
+    def see_if_die(true):
+        if true == False:
+            while random_time > 0:
+                it_is_red = input("").strip().upper()
+                time.sleep(1)
+                random_time = random_time-1
+        if it_is_red == "E":
+            died = True
+        else:
+            died = False
+        return died
+    if see_if_die(False) == True:
+        print("You died")
+        sys.exit()
+    for item in away:
+        print(item)
+    facing_away = True
+    print("PRESS E TO RUN")
+    e = input("").strip().upper()
+    if e == "E":
+        advanced_player = advanced_player+1
+    for item in towards:
+        print(item)
+    random_time = random.randint(1, 5)
+    print(f"Green light in {random_time}")
+    if see_if_die(False) == True:
+        print("You died")
+        sys.exit()
+    time.sleep(random_time)
+    for item in away:
+        print(item)
+    facing_away = True
+    print("PRESS E TO RUN")
+    e = input("").strip().upper()
+    if e == "E":
+        advanced_player = advanced_player+1
+    if advanced_player >= 3:
+        print("You win!")
+        sys.exit()
+    else:
+        doll()
 doll()
