07af0bb876de05a12af0375e6f6a7323a19710c8
DanTheShrimp <daniel.delong@ucas-edu.com>
2025-09-23 14:25:47 -0600

    Family looping in an endless circle

---
+++ Programs/Easy/my_family_loop.py
@@ -0,0 +1,13 @@
+#DD Period 7 My Family Loop
+
+import time
+family = ["Lizzie", "Sarah", "Dave"]
+slugs = ["Doomslug", "Chubs", "Fine", "Boomslug", "Bubbles", "Rose", "Boomerslug"]
+
+for member in family:
+    print(f"Hi, {member}! I am questing for a snack!")
+    time.sleep(1)
+
+for slug in slugs:
+    print(f"Awwwww, you're so cute, {slug}! Here, have a mushroom.")
+    time.sleep(1)
