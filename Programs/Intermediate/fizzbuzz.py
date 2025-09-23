3c031a9740cef7d43647fff2da441eec0b451e0f
DanTheShrimp <daniel.delong@ucas-edu.com>
2025-09-23 14:18:32 -0600

    FizzBuzz + Modulo = Great

---
+++ Programs/Intermediate/fizzbuzz.py
@@ -0,0 +1,14 @@
+#DD Period 7 FizzBuzz
+
+number = 0
+
+while number <= 49:
+    number += 1
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 5 == 0:
+        print("Buzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    else:
+        print(number)
