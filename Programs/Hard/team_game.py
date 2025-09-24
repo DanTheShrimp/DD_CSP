ce712b0442b47d8239eefbb57b2c116f93a658af
DanTheShrimp <daniel.delong@ucas-edu.com>
2025-09-24 14:41:07 -0600

    started team game

---
+++ Programs/Hard/team_game.py
@@ -0,0 +1,9 @@
+#DD Period 7 Build a Game
+
+# Daniel does monster encounter(s)
+# Shelby does the journey
+# Addie does endings
+# Elena does beginning (dialogue, input, etc.)
+
+# Storyline: Name the hero, then some old wizard asks for help because his magic something is missing oh no D:
+
---
+++ Programs/Hard/team_game_designs.py
@@ -0,0 +1,12 @@
+#DD Period 7 Build a Game monster designs
+
+def monsters(type):
+    goblin = [
+    "(\\___/)",
+    "| O O |",
+    "\\  ^  /",
+    " `````"
+    ]
+    for item in goblin:
+        print(item)
+monsters(1)
