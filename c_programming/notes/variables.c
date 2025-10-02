62fc3f55b06761b6035901a7a9a54f1fc0de64d3
DanTheShrimp <daniel.delong@ucas-edu.net>
2025-10-02 14:42:01 -0600

    variables

---
+++ c_programming/notes/variables.c
@@ -0,0 +1,14 @@
+// DD Period 7 Variables Notes
+#include <stdio.h>
+
+int main(void){
+    int grade = 95; // 4 bytes of space
+    float pi = 3.14; // 4 bytes of space
+    double long_pi = 3.1415926358; // 8 bytes of space
+    char letter_grade = 'A'; // 1 byte yey :D
+    char name[] = "Daniel";
+    printf("Gibbledy garbledy I cast 'RECEDING HAIRLINE'");
+    printf("%s killed Mr Johanson.", name);
+    printf("Because of that, %s got a %c in Chemistry!", name, letter_grade);
+    return 0;
+}
