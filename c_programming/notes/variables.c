6d8f546d86651b17f5eb97daad8645c1afb9a79e
DanTheShrimp <daniel.delong@ucas-edu.net>
2025-10-03 14:40:57 -0600

    notes are done :D

--- c_programming/notes/variables.c
+++ c_programming/notes/variables.c
@@ -7,11 +7,19 @@ int main(void){
     double long_pi = 3.1415926358; // 8 bytes of space
     char letter_grade = 'A'; // 1 byte yey :D
     char name[] = "Daniel";
+    char art[] = 
+    "/*****\\\n"
+    "| *_* |\n"
+    "\\_____/\n";
     char yes_no;
     printf("Gibbledy garbledy I cast 'RECEDING HAIRLINE'\n");
     printf("%s made Mr Johanson more bald.\n", name);
     printf("Because of that, %s got a %c in Chemistry!\n", name, letter_grade);
-    printf("Do you want to make him more bald.\n");
-    scanf("%c", &yes_no);
+    printf("Do you want to make him more bald?\n");
+    // user input
+    scanf("%s", yes_no);
+    printf("%s pulled out a razor and shaved his head clean!\n", name);
+    printf("%s", art);
     return 0;
 }
+// hi
