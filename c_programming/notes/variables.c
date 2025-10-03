e151c76ed4eaa2ada6d5d9022575cef334911a62
DanTheShrimp <daniel.delong@ucas-edu.net>
2025-10-03 14:25:43 -0600

    yes or no, no matter what Mr Johanson becomes balder

--- c_programming/notes/variables.c
+++ c_programming/notes/variables.c
@@ -2,13 +2,16 @@
 #include <stdio.h>
 
 int main(void){
-    int grade = 95; // 4 bytes of space
+    int grade; // 4 bytes of space
     float pi = 3.14; // 4 bytes of space
     double long_pi = 3.1415926358; // 8 bytes of space
     char letter_grade = 'A'; // 1 byte yey :D
     char name[] = "Daniel";
+    char yes_no;
     printf("Gibbledy garbledy I cast 'RECEDING HAIRLINE'\n");
     printf("%s made Mr Johanson more bald.\n", name);
     printf("Because of that, %s got a %c in Chemistry!\n", name, letter_grade);
+    printf("Do you want to make him more bald.\n");
+    scanf("%c", &yes_no);
     return 0;
 }
