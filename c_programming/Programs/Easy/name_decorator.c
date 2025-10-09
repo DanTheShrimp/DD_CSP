8b3b8c940dd993119d8ba2b39cf239ddab7da2a3
DanTheShrimp <daniel.delong@ucas-edu.net>
2025-10-09 14:41:49 -0600

    name decor started

---
+++ c_programming/Programs/Easy/name_decorator.c
@@ -0,0 +1,15 @@
+// DD Period 7 Name Decorator
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    char name[50];
+    char character[10];
+    printf("Good morning user. I am the Name Decorator 69000. Please give me your name. I promise I won't sell it ;)\n");
+    scanf("%s", name);
+    printf("Thank you, %s. Now give me characters to decorate your name with. Please keep your input to a maximum of 10 characters.\n", name);
+    scanf("%s", character);
+    printf("%s\n", character);
+    return 0;
+}
