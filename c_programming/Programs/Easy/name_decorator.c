3f7db39eca70eacc88c42fe6b43bee5c85965b6f
DanTheShrimp <daniel.delong@ucas-edu.net>
2025-10-10 14:23:06 -0600

    finished name decor

--- c_programming/Programs/Easy/name_decorator.c
+++ c_programming/Programs/Easy/name_decorator.c
@@ -6,10 +6,19 @@
 int main(void){
     char name[50];
     char character[10];
+    char finished_name[75];
     printf("Good morning user. I am the Name Decorator 69000. Please give me your name. I promise I won't sell it ;)\n");
     scanf("%s", name);
     printf("Thank you, %s. Now give me characters to decorate your name with. Please keep your input to a maximum of 10 characters.\n", name);
     scanf("%s", character);
-    printf("%s\n", character);
+
+    strcat(finished_name, character);
+    strcat(finished_name, " ");
+    strcat(finished_name, name);
+    strcat(finished_name, " ");
+    strcat(finished_name, character);
+
+    printf("Here is your finished name.\n");
+    printf("%s\n", finished_name);
     return 0;
 }
