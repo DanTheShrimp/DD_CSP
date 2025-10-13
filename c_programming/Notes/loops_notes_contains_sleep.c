9d5b9500dfa3ddab28401f15f9b7ec9fe8193433
DanTheShrimp <daniel.delong@ucas-edu.net>
2025-10-13 14:21:12 -0600

    loops notes

---
+++ c_programming/Notes/loops_notes_contains_sleep.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <windows.h> // we can access sleep() from this
+
+int main(void){
+    int numbs[] = {69, 42, 17, 89, 5, 21, 19}; //arrays, not lists
+    char candies[][20] = {"Twix", "Milky way", "Reeces"}; // we need a second bracket for the max length of a string
+    int heeheehee = 1;
+
+    for(int x=0; x<7; x++){
+        Sleep(1000); // basically time.sleep() in c, but the number is in milliseconds
+        printf("%d\n", numbs[x]);
+    }
+
+    for(int i=0; i<3; i++){
+        Sleep(1000);
+        printf("I like %s\n", candies[i]);
+    }
+
+    while(heeheehee>0){ // you cannot stop me from making an infinite loop >:)
+        printf("%d\n", heeheehee);
+        heeheehee++;
+        Sleep(10);
+    }
+    return 0;
+}
