79a139e18311d998b9b185c19e4adf4ad19b5f9f
DanTheShrimp <daniel.delong@ucas-edu.net>
2025-11-10 18:46:37 -0700

    thing

---
+++ c_programming/Programs/Easy/first_program.c
@@ -0,0 +1,10 @@
+// DD Period 7 First Program
+#include <stdio.h>
+
+int main(void){
+    char name[50];
+    printf("Hi! Oh, you aren't Spensa... Oh well! What's your name? I really like mushrooms.\n");
+    scanf("%s", name);
+    printf("Hello, %s! What a wonderful name! Would you like to go fly and maybe blow up?\n", name);
+    return 0;
+}
