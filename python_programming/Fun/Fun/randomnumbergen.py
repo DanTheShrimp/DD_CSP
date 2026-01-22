16fae4a4e7b4fd8b3a0d2b1eef276429841cbb37
Shrimp <daniel.delong@ucas-edu.net>
2026-01-22 22:32:12 +0000

    Finished all three, may want to add more

--- python_programming/Fun/Fun/randomnumbergen.py
+++ python_programming/Fun/Fun/randomnumbergen.py
@@ -1,5 +1,4 @@
 import random
-import sys
 import time
 
 def coin():
@@ -10,8 +9,9 @@ def coin():
         coinanswer=str("tails")
     else:
         print("Something went wrong.")
+    print("Flipping...")
     time.sleep(1)
-    print(f"The answer is {coinanswer}.")
+    print(f"The side is {coinanswer}.")
 
 def die():
     while True:
@@ -20,8 +20,18 @@ def die():
             print("Choose a number higher than 3.")
         else:
             break
+    time.sleep(0.5)
+    print("Rolling...")
     dieanswer=random.randint(1,sidesofdie)
-    print(f"The answer is {dieanswer}.")
+    time.sleep(1)
+    print(f"The number is {dieanswer}.")
+
+def card():
+    cardlist=["Ace of Spades","Two of Spades","Three of Spades","Four of Spades","Five of Spades","Six of Spades","Seven of Spades","Eight of Spades","Nine of Spades","Ten of Spades","Jack of Spades","Queen of Spades","King of Spades","Ace of Clubs","Two of Clubs","Three of Clubs","Four of Clubs","Five of Clubs","Six of Clubs","Seven of Clubs","Eight of Clubs","Nine of Clubs","Ten of Clubs","Jack of Clubs","Queen of Clubs","King of Clubs","Ace of Diamonds","Two of Diamonds","Three of Diamonds","Four of Diamonds","Five of Diamonds","Six of Diamonds","Seven of Diamonds","Eight of Diamonds","Nine of Diamonds","Ten of Diamonds","Jack of Diamonds","Queen of Diamonds","King of Diamonds","Ace of Hearts","Two of Hearts","Three of Hearts","Four of Hearts","Five of Hearts","Six of Hearts","Seven of Hearts","Eight of Hearts","Nine of Hearts","Ten of Hearts","Jack of Hearts","Queen of Hearts","King of Hearts","Joker","Joker"]
+    print("Drawing a random card...")
+    cardanswer=random.randint(1,54)
+    time.sleep(1)
+    print(f"The card is {cardlist[cardanswer]}.")
 
 def menu():
     menu=[
@@ -33,10 +43,19 @@ def menu():
     for item in menu:
         print(item)
         time.sleep(1)
-    ques1=str(input("Which of the options above would you like to use?\n"))
-    if "coin" in ques1:
-        coin()
-    elif "die" in ques1 or "dice" in ques1:
-        die()
+    while True:
+        ques1=str(input("Which of the options above would you like to use?\n"))
+        if "coin" in ques1:
+            coin()
+            break
+        elif "die" in ques1 or "dice" in ques1:
+            die()
+            break
+        elif "card" in ques1:
+            card()
+            break
+        else:
+            print("Choose a valid option.")
+            time.sleep(0.5)
 
 menu()
