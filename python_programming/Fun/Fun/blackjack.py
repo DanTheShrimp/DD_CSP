f1fe017ede01d1cd0f35fbaac3d0e4454d8b8aa6
DanTheShrimp <daniel.delong@ucas-edu.net>
2026-01-24 19:45:36 -0700

    Got splitting to display value

--- python_programming/Fun/Fun/blackjack.py
+++ python_programming/Fun/Fun/blackjack.py
@@ -378,6 +378,14 @@ deck=[
     "|  %%%|",
     "|_%%%>|"],
 ]
+blank=[
+    "       ",
+    "       ",
+    "       ",
+    "       ",
+    "       ",
+    "       "
+]
 house_hand=[]
 player_hand=[]
 unknown_card=random.randint(1,52)
@@ -407,6 +415,7 @@ elif "10" in house_hand[0][1]:
     uc_value=10
 
 player_card0=random.randint(1,51)
+player_card0=2
 player_hand.append(deck[player_card0])
 del deck[player_card0]
 if "A" in player_hand[0][1]:
@@ -459,6 +468,7 @@ elif "10" in house_hand[1][1]:
     hc0_value=10
 
 player_card1=random.randint(1,49)
+player_card1=14
 player_hand.append(deck[player_card1])
 del deck[player_card1]
 if "A" in player_hand[1][1]:
@@ -485,23 +495,127 @@ elif "10" in player_hand[1][1]:
     pc1_value=10
 
 def player_part():
+    split_able="no"
     if pc0_value=="A" and pc1_value=="A":
-        split_able="yes"
         typer(f"Your total card value is 2 or 12.")
     elif pc0_value=="A":
         if pc1_value+11==21:
             typer("Blackjack! You win.")
+            sys.exit()
         else:
             typer(f"Your total card value is {pc1_value+1} or {pc1_value+11}")
     elif pc1_value=="A":
         if pc0_value+11==21:
             typer("Blackjack! You win.")
-        typer(f"Your total card value is {pc0_value+1} or {pc0_value+11}")
+            sys.exit()
+        else:
+            typer(f"Your total card value is {pc0_value+1} or {pc0_value+11}")
     else:
-        typer(f"Your total card valie is {pc0_value+pc1_value}")
+        typer(f"Your total card value is {pc0_value+pc1_value}")
     if pc0_value==pc1_value:
         split_able="yes"
-    
+    while True:
+        if split_able=="yes":
+            will_split=input("You are able to split because you're card values are equal. Do you want to split?\n")
+            if "yes" in will_split:
+                typer("Hitting on both.")
+                typer("House's cards: Your cards:")
+                time.sleep(0.5)
+                player_card2=random.randint(1,48)
+                player_hand.append(deck[player_card2])
+                del deck[player_card2]
+                if "A" in player_hand[2][1]:
+                    pc2_value="A"
+                elif "J" in player_hand[2][1] or "Q" in player_hand[2][1] or "K" in player_hand[2][1]:
+                    pc2_value=10
+                elif "2" in player_hand[2][1]:
+                    pc2_value=2
+                elif "3" in player_hand[2][1]:
+                    pc2_value=3
+                elif "4" in player_hand[2][1]:
+                    pc2_value=4
+                elif "5" in player_hand[2][1]:
+                    pc2_value=5
+                elif "6" in player_hand[2][1]:
+                    pc2_value=6
+                elif "7" in player_hand[2][1]:
+                    pc2_value=7
+                elif "8" in player_hand[2][1]:
+                    pc2_value=8
+                elif "9" in player_hand[2][1]:
+                    pc2_value=9
+                elif "10" in player_hand[2][1]:
+                    pc2_value=10
+
+                player_card3=random.randint(1,47)
+                player_hand.append(deck[player_card3])
+                del deck[player_card3]
+                if "A" in player_hand[3][1]:
+                    pc3_value="A"
+                elif "J" in player_hand[3][1] or "Q" in player_hand[3][1] or "K" in player_hand[3][1]:
+                    pc3_value=10
+                elif "2" in player_hand[3][1]:
+                    pc3_value=2
+                elif "3" in player_hand[3][1]:
+                    pc3_value=3
+                elif "4" in player_hand[3][1]:
+                    pc3_value=4
+                elif "5" in player_hand[3][1]:
+                    pc3_value=5
+                elif "6" in player_hand[3][1]:
+                    pc3_value=6
+                elif "7" in player_hand[3][1]:
+                    pc3_value=7
+                elif "8" in player_hand[3][1]:
+                    pc3_value=8
+                elif "9" in player_hand[3][1]:
+                    pc3_value=9
+                elif "10" in player_hand[3][1]:
+                    pc3_value=10
+                printer_helper=0
+                for item in deck[0]:
+                    print(f" {item}        {player_hand[0][printer_helper]}   {player_hand[1][printer_helper]}")
+                    printer_helper+=1
+                printer_helper=0
+                for item in blank:
+                    print(f" {item}        {player_hand[2][printer_helper]}   {player_hand[3][printer_helper]}")
+                    printer_helper+=1
+                if pc0_value=="A" and pc2_value=="A":
+                    typer(f"Your total card value for pair one is 2 or 12.")
+                elif pc2_value=="A":
+                    if pc0_value+11==21:
+                        typer("Blackjack! That pair is now locked.")
+                        pair1_locked="yes"
+                    else:
+                        typer(f"Your total card value for pair one is {pc0_value+1} or {pc0_value+11}.")
+                        pair1_locked="no"
+                elif pc0_value=="A":
+                    if pc2_value+11==21:
+                        typer("Blackjack! That pair is now locked.")
+                        pair1_locked="yes"
+                    else:
+                        typer(f"Your total card value for pair one is {pc2_value+1} or {pc2_value+11}.")
+                        pair1_locked="no"
+                elif pc1_value=="A" and pc3_value=="A":
+                    typer(f"Your total card value for pair two is 2 or 12.")
+                elif pc3_value=="A":
+                    if pc1_value+11==21:
+                        typer("Blackjack! That pair is now locked.")
+                        pair2_locked="yes"
+                    else:
+                        typer(f"Your total card value is {pc1_value+1} or {pc1_value+11}.")
+                        pair2_locked="no"
+                elif pc1_value=="A":
+                    if pc3_value+11==21:
+                        typer("Blackjack! That pair is now locked.")
+                        pair2_locked="yes"
+                    else:
+                        typer(f"Your total card value is {pc3_value+1} or {pc3_value+11}.")
+                        pair2_locked="no"
+                else:
+                    typer(f"Your total card value for pair one is {pc0_value+pc2_value}.")
+                    typer(f"Your total card value for pair two is {pc1_value+pc3_value}.")
+        break
 
 def menu():
     typer("Welcome to Blackjack.")
